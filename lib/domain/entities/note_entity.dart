d67240d73b521dba92d0c65f734e296f97186f8c
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-05-06 12:05:10 +0300

    feat: Created entity

---
+++ lib/domain/entities/note_entity.dart
@@ -0,0 +1,13 @@
+class NoteEntity {
+  final int id;
+  final String title;
+  final String content;
+  final String image;
+
+  const NoteEntity({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.image,
+  });
+}

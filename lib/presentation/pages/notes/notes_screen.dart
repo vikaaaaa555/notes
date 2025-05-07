a8c45ab71f57f64873e6776dca40dbe7925aa399
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-05-07 18:28:07 +0300

    feat: Added dialog actions to delete & update notes

--- lib/presentation/pages/notes/notes_screen.dart
+++ lib/presentation/pages/notes/notes_screen.dart
@@ -1,8 +1,11 @@
-import 'package:dog_journal/presentation/pages/notes/widgets/add_note.dart';
+import 'package:dog_journal/presentation/pages/notes/widgets/actions/add_note.dart';
+import 'package:dog_journal/presentation/pages/notes/widgets/actions/update_note.dart';
 import 'package:dog_journal/presentation/pages/notes/widgets/note_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../domain/entities/note_entity.dart';
+import '../../bloc/notes_bloc.dart';
 
 class NotesScreen extends StatefulWidget {
   final List<NoteEntity?> notes;
@@ -14,6 +17,50 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+  void _showOptionsDialog(NoteEntity note) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Center(
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed:
+                          () => showModalBottomSheet(
+                            context: context,
+                            builder:
+                                (context) => Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: UpdateNote(note: note),
+                                ),
+                          ),
+                      child: Text('Update'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        context.read<NotesBloc>().add(
+                          DeleteNoteEvent(id: note.id),
+                        );
+                        Navigator.pop(context);
+                      },
+                      child: Text('Delete'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,10 +83,13 @@ class _NotesScreenState extends State<NotesScreen> {
                   itemCount: widget.notes.length,
                   itemBuilder: (context, index) {
                     final note = widget.notes[index];
-                    return NoteCard(
-                      title: note!.title,
-                      content: note.content,
-                      image: note.image,
+                    return GestureDetector(
+                      onLongPress: () => _showOptionsDialog(note),
+                      child: NoteCard(
+                        title: note!.title,
+                        content: note.content,
+                        image: note.image,
+                      ),
                     );
                   },
                 ),

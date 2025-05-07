cc093af40a825012fceca543faa1189a5474d26b
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-05-07 18:27:37 +0300

    feat: Created an update note action

---
+++ lib/presentation/pages/notes/widgets/actions/update_note.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../domain/entities/note_entity.dart';
+import '../../../../bloc/notes_bloc.dart';
+import '../note_form.dart';
+
+class UpdateNote extends StatelessWidget {
+  final NoteEntity note;
+
+  const UpdateNote({super.key, required this.note});
+
+  @override
+  Widget build(BuildContext context) {
+    return NoteForm(
+      initialTitle: note.title,
+      initialContent: note.content,
+      initialImagePath: note.image,
+      onSubmit: (title, content, imagePath) {
+        context.read<NotesBloc>().add(
+          UpdateNoteEvent(
+            id: note.id,
+            title: title,
+            content: content,
+            image: imagePath,
+          ),
+        );
+        Navigator.pop(context);
+      },
+    );
+  }
+}

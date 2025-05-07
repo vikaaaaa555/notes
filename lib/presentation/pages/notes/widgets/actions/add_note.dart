3e374162f215f20b500bec5f3839a9d9f244708e
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-05-07 18:00:41 +0300

    refactoring: Moved to actions dir

--- lib/presentation/pages/notes/widgets/actions/add_note.dart
+++ lib/presentation/pages/notes/widgets/actions/add_note.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
-import '../../../bloc/notes_bloc.dart';
+import '../../../../bloc/notes_bloc.dart';
 
 class AddNote extends StatefulWidget {
   const AddNote({super.key});
